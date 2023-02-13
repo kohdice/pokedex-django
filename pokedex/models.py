555912c3636b3d00fefdafc95f91915294c523ce
kohdice <kohdice@gmail.com>
2023-02-13 13:44:30 +0000

    app pokedexのmodelを修正

--- pokedex/models.py
+++ pokedex/models.py
@@ -7,30 +7,23 @@ class NationalPokedex(models.Model):
         db_table = "national_pokedex"
 
     number = models.PositiveSmallIntegerField(
-        verbose_name="Pokedex number", primary_key=True
+        verbose_name="National Pokedex number", primary_key=True
     )
     name = models.CharField(
         verbose_name="Pokemon Name", max_length=12, unique=True
     )
-    created_by = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class GenderMaster(models.Model):
-    class Meta:
-        db_table = "gender_mst"
-
-    gender = models.CharField(
-        verbose_name="Gender",
-        max_length=2,
-        primary_key=True
+    created_by = models.CharField(
+        verbose_name="Created by", max_length=255
     )
-    created_by = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(
+        verbose_name="Created at", auto_now_add=True
+    )
+    updated_by = models.CharField(
+        verbose_name="Updated by", max_length=255
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Updated by", auto_now=True
+    )
 
 
 class TypeMaster(models.Model):
@@ -40,99 +33,29 @@ class TypeMaster(models.Model):
     type = models.CharField(
         verbose_name="Type", max_length=5, primary_key=True
     )
-    created_by = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class AbilityMaster(models.Model):
-    class Meta:
-        db_table = "ability_mst"
-
-    ability = models.CharField(
-        verbose_name="Ability", max_length=8, primary_key=True
+    created_by = models.CharField(
+        verbose_name="Created by", max_length=255
     )
-    created_by = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class FormMaster(models.Model):
-    class Meta:
-        db_table = "forms_mst"
-
-    forms = models.CharField(
-        verbose_name="forms", max_length=10, primary_key=True
+    created_at = models.DateTimeField(
+        verbose_name="Created at", auto_now_add=True
     )
-    created_by = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class RegionMaster(models.Model):
-    class Meta:
-        db_table = "region_mst"
-
-    region = models.CharField(
-        verbose_name="Region", max_length=4, primary_key=True
+    updated_by = models.CharField(
+        verbose_name="Updated by", max_length=255
     )
-    created_by = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(
+        verbose_name="Updated by", auto_now=True
+    )
 
 
-class PokemonStatus(models.Model):
+class PokemonType(models.Model):
     class Meta:
-        db_table = "pokemon_status"
+        db_table = "pokemon_type"
 
     number = models.ForeignKey(
         NationalPokedex,
-        verbose_name="National Pokedex Number",
-        on_delete=models.PROTECT,
-    )
-    gender = models.ForeignKey(
-        GenderMaster,
-        verbose_name="Gender",
-        null=True,
-        default=None,
+        verbose_name="National Pokemon Number",
         on_delete=models.PROTECT
     )
-    forms = models.ForeignKey(
-        FormMaster,
-        verbose_name="Forms",
-        null=True,
-        default=None,
-        on_delete=models.PROTECT,
-    )
-    regional_variant = models.ForeignKey(
-        RegionMaster,
-        verbose_name="Regional variant",
-        null=True,
-        default=None,
-        on_delete=models.PROTECT,
-    )
-    mega_evolution = models.BooleanField(
-        verbose_name="Mega Evolution", default=False
-    )
-    primal_reversion = models.BooleanField(
-        verbose_name="Primal Reversion", default=False
-    )
-    hp = models.PositiveSmallIntegerField(verbose_name="Hit Point")
-    attack = models.PositiveSmallIntegerField(
-        verbose_name="Attack",
-    )
-    defense = models.PositiveSmallIntegerField(verbose_name="Defense")
-    special_attack = models.PositiveSmallIntegerField(
-        verbose_name="Special Attack"
-    )
-    special_defense = models.PositiveSmallIntegerField(
-        verbose_name="Special Defense"
-    )
-    speed = models.PositiveSmallIntegerField(verbose_name="Speed")
     type_1 = models.ForeignKey(
         TypeMaster,
         verbose_name="Type 1",
@@ -147,7 +70,15 @@ class PokemonStatus(models.Model):
         default=False,
         on_delete=models.PROTECT,
     )
-    created_by = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_by = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.CharField(
+        verbose_name="Created by", max_length=255
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Created at", auto_now_add=True
+    )
+    updated_by = models.CharField(
+        verbose_name="Updated by", max_length=255
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Updated by", auto_now=True
+    )
